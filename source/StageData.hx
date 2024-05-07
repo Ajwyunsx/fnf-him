3a96f58e4e7c2eb1fab44fc99de3b637c04e7ffd
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-05-07 15:40:32 +0800

    Add files via upload

--- source/StageData.hx
+++ source/StageData.hx
@@ -1,21 +1,19 @@
 package;
 
 #if MODS_ALLOWED
-import sys.FileSystem;
 import sys.io.File;
+import sys.FileSystem;
 #end
 import openfl.utils.Assets;
-import Song;
 import haxe.Json;
 import haxe.format.JsonParser;
+import Song;
 
 using StringTools;
 
-typedef StageFile =
-{
+typedef StageFile = {
 	var directory:String;
 	var defaultZoom:Float;
-	var defaultExtend:Float;
 	var isPixelStage:Bool;
 
 	var boyfriend:Array<Dynamic>;
@@ -23,53 +21,64 @@ typedef StageFile =
 	var opponent:Array<Dynamic>;
 }
 
-class StageData
-{
+class StageData {
 	public static var forceNextDirectory:String = null;
-
-	public static function loadDirectory(SONG:SwagSong)
-	{
+	public static function loadDirectory(SONG:SwagSong) {
 		var stage:String = '';
-		if (SONG.stage != null)
-		{
+		if(SONG.stage != null) {
 			stage = SONG.stage;
-		}
-		else
-		{
+		} else if(SONG.song != null) {
+			switch (SONG.song.toLowerCase().replace(' ', '-'))
+			{
+				case 'spookeez' | 'south' | 'monster':
+					stage = 'spooky';
+				case 'pico' | 'blammed' | 'philly' | 'philly-nice':
+					stage = 'philly';
+				case 'milf' | 'satin-panties' | 'high':
+					stage = 'limo';
+				case 'cocoa' | 'eggnog':
+					stage = 'mall';
+				case 'winter-horrorland':
+					stage = 'mallEvil';
+				case 'senpai' | 'roses':
+					stage = 'school';
+				case 'thorns':
+					stage = 'schoolEvil';
+				default:
+					stage = 'stage';
+			}
+		} else {
 			stage = 'stage';
 		}
 
 		var stageFile:StageFile = getStageFile(stage);
-		if (stageFile == null)
-		{ // preventing crashes
+		if(stageFile == null) { //preventing crashes
 			forceNextDirectory = '';
-		}
-		else
-		{
+		} else {
 			forceNextDirectory = stageFile.directory;
 		}
 	}
 
-	public static function getStageFile(stage:String):StageFile
-	{
+	public static function getStageFile(stage:String):StageFile {
 		var rawJson:String = null;
 		var path:String = Paths.getPreloadPath('stages/' + stage + '.json');
 
 		#if MODS_ALLOWED
 		var modPath:String = Paths.modFolders('stages/' + stage + '.json');
-		if (FileSystem.exists(modPath))
-		{
+		if(FileSystem.exists(modPath)) {
 			rawJson = File.getContent(modPath);
+		} else if(FileSystem.exists(path)) {
+			rawJson = File.getContent(path);
 		}
-		#end
-		if (Assets.exists(path))
-		{
+		#else
+		if(Assets.exists(path)) {
 			rawJson = Assets.getText(path);
 		}
-	else
-	{
-		return null;
-	}
+		#end
+		else
+		{
+			return null;
+		}
 		return cast Json.parse(rawJson);
 	}
 }
