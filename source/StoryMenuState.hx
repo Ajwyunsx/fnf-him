be79cd10e8623e9fb773f4e273c877ede4e5b220
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-08-08 13:56:11 +0800

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -51,9 +51,7 @@ class StoryMenuState extends MusicBeatState
 
 	override function create()
 	{
-		#if MODS_ALLOWED
-		Paths.destroyLoadedImages();
-		#end
+	
 		WeekData.reloadWeekFiles(true);
 		if(curWeek >= WeekData.weeksList.length) curWeek = 0;
 		persistentUpdate = persistentDraw = true;
