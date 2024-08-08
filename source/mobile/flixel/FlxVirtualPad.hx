98f2a478a10bb2c5e3cfd930913e353d64b9e7c4
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-08-08 13:19:00 +0800

    Update FlxVirtualPad.hx

--- source/mobile/flixel/FlxVirtualPad.hx
+++ source/mobile/flixel/FlxVirtualPad.hx
@@ -9,30 +9,7 @@ import flixel.math.FlxPoint;
 import mobile.flixel.FlxButton;
 import openfl.utils.Assets;
 
-enum FlxDPadMode
-{
-	UP_DOWN;
-	LEFT_RIGHT;
-	UP_LEFT_RIGHT;
-	LEFT_FULL;
-	RIGHT_FULL;
-	BOTH_FULL;
-	NONE;
-}
 
-enum FlxActionMode
-{
-	A;
-	B;
-	A_B;
-	A_B_C;
-	A_B_E;
-	A_B_X_Y;
-	A_B_C_X_Y;
-	A_B_C_X_Y_Z;
-	A_B_C_D_V_X_Y_Z;
-	NONE;
-}
 
 /**
  * A gamepad.
@@ -200,4 +177,28 @@ class FlxVirtualPad extends FlxSpriteGroup
 		#end
 		return button;
 	}
+}
+enum FlxDPadMode
+{
+	UP_DOWN;
+	LEFT_RIGHT;
+	UP_LEFT_RIGHT;
+	LEFT_FULL;
+	RIGHT_FULL;
+	BOTH_FULL;
+	NONE;
+}
+
+enum FlxActionMode
+{
+	A;
+	B;
+	A_B;
+	A_B_C;
+	A_B_E;
+	A_B_X_Y;
+	A_B_C_X_Y;
+	A_B_C_X_Y_Z;
+	A_B_C_D_V_X_Y_Z;
+	NONE;
 }
