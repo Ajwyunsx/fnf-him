f72bbfb4a289a7ce2c7ce031557c55bad839dec3
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2024-08-08 16:40:45 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -271,11 +271,8 @@ class FreeplayState extends MusicBeatState
 		{
 			var songLowercase:String = Paths.formatToSongPath(songs[curSelected].songName);
 			var poop:String = Highscore.formatSong(songLowercase, curDifficulty);
-			#if MODS_ALLOWED
-			if(!sys.FileSystem.exists(Paths.modsJson(songLowercase + '/' + poop)) && !sys.FileSystem.exists(Paths.json(songLowercase + '/' + poop))) {
-			#else
+			
 			if(!OpenFlAssets.exists(Paths.json(songLowercase + '/' + poop))) {
-			#end
 				poop = songLowercase;
 				curDifficulty = 1;
 				trace('Couldnt find file');
@@ -307,11 +304,7 @@ class FreeplayState extends MusicBeatState
 		{
 			var songLowercase:String = Paths.formatToSongPath(songs[curSelected].songName);
 			var poop:String = Highscore.formatSong(songLowercase, curDifficulty);
-			#if MODS_ALLOWED
-			if(!sys.FileSystem.exists(Paths.modsJson(songLowercase + '/' + poop)) && !sys.FileSystem.exists(Paths.json(songLowercase + '/' + poop))) {
-			#else
 			if(!OpenFlAssets.exists(Paths.json(songLowercase + '/' + poop))) {
-			#end
 				poop = songLowercase;
 				curDifficulty = 1;
 				trace('Couldnt find file');
